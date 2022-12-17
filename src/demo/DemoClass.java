1f8dbf9351ee426afc1b5b53ca9f47727372fb87
ankit <ankitkeshri889>
2022-12-17 19:42:31 +0530

    First Commit

---
+++ src/demo/DemoClass.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class DemoClass {
+	
+	String name;
+	String email;
+	int age;
+	
+	
+	public static void main(String[] args) {
+		DemoClass demo = new DemoClass();
+		demo.name = "Marshal";
+		System.out.println(demo.name);
+	}
+
+}
