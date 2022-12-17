512d245b3083a866da03abaac2be2f2e3d9a86fe
ankit <ankitkeshri889>
2022-12-17 19:46:28 +0530

    Second Commit added Email and age of Person

--- src/demo/DemoClass.java
+++ src/demo/DemoClass.java
@@ -11,6 +11,11 @@ public class DemoClass {
 		DemoClass demo = new DemoClass();
 		demo.name = "Marshal";
 		System.out.println(demo.name);
+		
+		demo.email = "ankit@gmail.com";
+		demo.age = 24;
+		
+		System.out.println(demo.email +" "+demo.age);
 	}
 
 }
